59abaf4bcd7f0e0171ab3eb099df91c972ff9eae
Meder <m.toktosunov@icloud.com>
2018-11-27 21:27:05 -0500

    renamed smart_vending_machine.rb to vendor.rb

---
+++ vendor.rb
@@ -0,0 +1,33 @@
+
+
+class Vendor
+  def initialize()
+
+end
+#
+# # prompting user to input shopping item
+# puts "Hello, what would you like today?"
+# item_name = gets.chomp
+#
+# # prompting user to input item quantity
+# puts "How many items of #{item_name} would you like?"
+# item_quantity = gets.chomp
+#
+# # outputing the result upon conditionals
+# if item_quantity.index('tons')
+#   while true do
+#     rand_num = Random.rand(15)
+#     rand_num.times do
+#       puts item_name
+#     end
+#     break if rand_num == 0
+#   end
+#   puts "Here you go! Come to try something new next time!"
+# elsif item_quantity.to_i > 0
+#   item_quantity.to_i.times do
+#     puts item_name
+#   end
+#   puts "Here you go! Come to try something new next time!"
+# else
+#   puts "Invalid input! Please, try again!"
+# end
